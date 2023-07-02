325b864d22064f651eaf8f532cd8565caa656347
alyadyh <alya.mardhiyyah@binus.ac.id>
2023-07-02 22:03:29 +0700

    display hairstyles based on categories

--- resources/views/display/hairstyles/show.blade.php
+++ resources/views/display/hairstyles/show.blade.php
@@ -8,7 +8,7 @@
     <div class="col-lg-4 col-md-6">
         <div class="single_service">
              <div class="service_thumb">
-                 <img src="{{ asset('storage/images/hairstyles/'.$item->image) }}" class="img-fluid" alt="">
+                 <img src="{{ asset('storage/images/hairstyles/'.$item->image) }}" class="img-fluid" alt="" style="height: 4in">
              </div>
              <div class="service_content text-center">
                 <h3>{{ $item->name }}</h3>
